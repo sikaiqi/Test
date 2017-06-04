5fe910160eff62e585da88585aa70ce712accc2f
sikaiqi <sikaiqi1992@163.com>
2017-06-04 12:10:08 +0800

    add project Test

---
+++ src/com/example/Demo.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		String format = String.format("/%s/%s", "aa", "bb");
+		System.out.println(format);
+	}
+	
+
+}
